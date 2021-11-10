6412f71a30715018a058985801c2f865d0610772
yelby <89603526+HuskieCat@users.noreply.github.com>
2021-11-10 20:05:23 +0000

    Added player choose X or O

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/*
+To-do list
+Play function to actually... play
+win function
+print a more real looking tik-tak-toe board
+allow user to choose location
+allow user to choose X or O for character
+make a computer opponent
+check to make sure all requirements are met for the project
+*/
+
 console.log("~~~Program Start~~~");
 
 var boardSize = 0;
@@ -5,6 +16,10 @@ getBoardSize();
 console.log("Your board size is " + boardSize);
 var board = generateEmptyBoard(boardSize);
 printBoard(board);
+
+let input = '';
+getCharacter();
+console.log("Player character is " + input);
 
 console.log("~~~Program End~~~");
 
@@ -27,16 +42,55 @@ function getBoardSize()
 
 function generateEmptyBoard(boardSize)
 {
-  var board = new Array(boardSize);
+  let board = new Array(boardSize);
 
-  for(var i = 0; i < board.length; i++)
+  for(let i = 0; i < board.length; i++)
     board[i] = new Array(boardSize);
+
+  fillBlank(board);
   return board;
 }
 
 function printBoard(board)
 {
-  for(var i = 0; i < board.length; i++)
-    console.log(board[i]);
+  for(let row = 0; row < board.length; row++)
+  {
+    for(let column = 0; column < board[row].length; column++)
+    {
+      process.stdout.write(board[row][column] + "");
+      if(column < board[row].length - 1)
+        process.stdout.write(" | ");
+    }
+    console.log("");
+  }
   return;
+}
+
+function fillBlank(board)
+{
+  for(let row = 0; row < board.length; row++)
+  {
+    for(let column = 0; column < board[row].length; column++)
+    {
+     board[row][column] = " " ;
+    }
+  }
+  return;
+}
+
+function getCharacter()
+{
+  while(true)
+  {
+    let inputPrompt = prompt("Please select X or O:");
+    input = String(inputPrompt);
+
+    switch(input)
+    {
+      case "o": case "O": case "x": case "X":
+        return;
+    }
+
+    console.log("Player character is invalid, please try again.")
+  }
 }
--- ttt_project.js
+++ ttt_project.js
@@ -7,6 +7,11 @@ let boardSizePrompt = prompt("Please give a board size:");
 getBoardSize();
 console.log("Your board size is " + boardSize);
 
+let input = '';
+let boardSizePrompt = prompt("Please select X or O:");
+getPiece();
+console.log("You are player " + input);
+
 console.log("~~~Program End~~~");
 
 
@@ -22,4 +27,15 @@ function getBoardSize()
     else
       return;
   }
+}
+
+function getPiece()
+{
+  let inputPrompt = prompt("Please select X or O:");
+  input = String(inputPrompt);
+  if(input = 'x' || input = 'X')
+    console.log(" You are player X.");
+  else
+    console.log("You are player O.");
+  return;
 }
